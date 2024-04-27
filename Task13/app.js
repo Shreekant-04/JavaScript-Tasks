a1d8e879e086bf766ac983ce4d0a1ed553717633
Shreekant <shree.kant.74.sk@gmail.com>
2024-04-27 11:51:53 +0530

    task 13 edited app.js

--- Task13/app.js
+++ Task13/app.js
@@ -5,40 +5,40 @@ let arrays = [
   [101, 112, 149, 79, 121, 54, 89, 95, 119, 122],
   [268, 265, 134, 262, 234, 129, 200, 144, 236, 174],
 ];
+let arr;
+//Max number
+function max() {
+  let num = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > num) {
+      num = arr[i];
+    }
+  }
+  console.log("Maximum number:-", num);
+}
+// sum of all element
+const sum = () => {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  console.log("Sum of all elements: -", total);
+};
+
+//odd number count
+const count = function () {
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] % 2 !== 0) {
+      count++;
+    }
+  }
+  console.log("Count of odd numbers: -", count);
+};
 for (let i = 0; i < arrays.length; i++) {
   arr = arrays[i];
-  //Max number
-  function max() {
-    let num = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > num) {
-        num = arr[i];
-      }
-    }
-    console.log("Maximum number:-", num);
-  }
-  // sum of all element
-  const sum = () => {
-    let total = 0;
-    for (let i = 0; i < arr.length; i++) {
-      total += arr[i];
-    }
-    console.log("Sum of all elements: -", total);
-  };
-
-  //odd number count
-  const count = function () {
-    let count = 0;
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] % 2 !== 0) {
-        count++;
-      }
-    }
-    console.log("Count of odd numbers: -", count);
-  };
-
   //call
-  console.log("Array : -", arrays[i]);
+  console.log(`Array : -[${arr.toString()}] `);
   max();
   sum();
   count();
