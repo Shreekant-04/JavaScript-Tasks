3d16710ef9322e536077b302bf51c8e69e1e06ce
Shreekant <shree.kant.74.sk@gmail.com>
2024-04-27 10:11:45 +0530

    even/odd added text edited

--- compoundInterest/app.js
+++ compoundInterest/app.js
@@ -14,21 +14,21 @@ let d = 2;
 if (a % 2 === 0) {
   console.log(`The number ${a} is even`);
 } else {
-  console.log(`The number ${a} is oddd`);
+  console.log(`The number ${a} is odd`);
 }
 if (b % 2 === 0) {
   console.log(`The number ${b} is even`);
 } else {
-  console.log(`The number ${b} is oddd`);
+  console.log(`The number ${b} is odd`);
 }
 
 if (c % 2 === 0) {
   console.log(`The number ${c} is even`);
 } else {
-  console.log(`The number ${c} is oddd`);
+  console.log(`The number ${c} is odd`);
 }
 if (d % 2 === 0) {
   console.log(`The number ${d} is even`);
 } else {
-  console.log(`The number ${d} is oddd`);
+  console.log(`The number ${d} is odd`);
 }
