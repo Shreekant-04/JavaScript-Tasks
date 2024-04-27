0d0704b3f332922ce29ae39636d1086a7ca60a59
Shreekant <shree.kant.74.sk@gmail.com>
2024-04-27 10:07:27 +0530

    even/odd added

--- compoundInterest/app.js
+++ compoundInterest/app.js
@@ -1,7 +1,34 @@
-let p = 2000;
-let r = 18 / 100;
-let t = 3;
-let n = 1;
+// let p = 2000;
+// let r = 18 / 100;
+// let t = 3;
+// let n = 1;
 
-let amount = p *((1 + r / n) ** (n * t));
-console.log(`The compound interest after ${t} years is : ${amount.toFixed(2)}`);
+// let amount = p *((1 + r / n) ** (n * t));
+// console.log(`The compound interest after ${t} years is : ${amount.toFixed(2)}`);
+
+let a = 5;
+let b = 6;
+let c = 7;
+let d = 2;
+
+if (a % 2 === 0) {
+  console.log(`The number ${a} is even`);
+} else {
+  console.log(`The number ${a} is oddd`);
+}
+if (b % 2 === 0) {
+  console.log(`The number ${b} is even`);
+} else {
+  console.log(`The number ${b} is oddd`);
+}
+
+if (c % 2 === 0) {
+  console.log(`The number ${c} is even`);
+} else {
+  console.log(`The number ${c} is oddd`);
+}
+if (d % 2 === 0) {
+  console.log(`The number ${d} is even`);
+} else {
+  console.log(`The number ${d} is oddd`);
+}
